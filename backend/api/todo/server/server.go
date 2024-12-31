b26280377e87dda1ccbede8e273a16aabcb92bd7
Christian <MrBanditHat@gmail.com>
2024-12-31 16:31:26 -0700

    Fixed an issue where the client requests runnin on server 3000 were being blocked by CORS policies. Adjusted the 'server.go' file to permit requests from the aformentioned server.

--- backend/api/todo/server/server.go
+++ backend/api/todo/server/server.go
@@ -1,17 +1,36 @@
 package server
 
 import (
-	"github.com/keyglee/assessment/api/router"
-	"github.com/keyglee/assessment/api/todo/handler"
-	"github.com/labstack/echo/v4"
+    "github.com/keyglee/assessment/api/router"
+    "github.com/keyglee/assessment/api/todo/handler"
+    "github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4/middleware"
 )
 
 func CreateServer(props *router.RouterProps) *echo.Echo {
-	r := router.New()
+    r := router.New()
 
-	h := handler.NewHandler()
+    // Enable CORS for localhost:3000 
+    r.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+        AllowOrigins: []string{"http://localhost:3000"},
+        AllowMethods: []string{
+            echo.GET,
+            echo.HEAD,
+            echo.PUT,
+            echo.PATCH,
+            echo.POST,
+            echo.DELETE,
+        },
+        AllowHeaders: []string{
+            echo.HeaderOrigin,
+            echo.HeaderContentType,
+            echo.HeaderAccept,
+            echo.HeaderAuthorization,
+        },
+    }))
 
-	h.RegisterRoutes(r.Group(props.BasePath))
+    h := handler.NewHandler()
+    h.RegisterRoutes(r.Group(props.BasePath))
 
-	return r
+    return r
 }
